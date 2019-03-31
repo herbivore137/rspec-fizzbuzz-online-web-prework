f5e448df34d4c213a515075f97f913a11fdb4043
B.A. Hanlon <herbivore137@gmail.com>
2019-03-31 20:40:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,14 @@
 
 def fizzbuzz(int)
   
-  if int % 3 == 0
+  if int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
+  elsif int % 3 == 0 
     "Fizz"
   elsif int % 5 == 0 
     "Buzz"
+  else
+    return nil;
       
   end
     
