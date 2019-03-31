2b4d1c6e68733ea003ba14554621f98b0bc2849f
B.A. Hanlon <herbivore137@gmail.com>
2019-03-31 20:41:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,4 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
-
-
 def fizzbuzz(int)
-  
   if int % 3 == 0 && int % 5 == 0 
     "FizzBuzz"
   elsif int % 3 == 0 
@@ -12,7 +7,5 @@ def fizzbuzz(int)
     "Buzz"
   else
     return nil;
-      
   end
-    
 end
